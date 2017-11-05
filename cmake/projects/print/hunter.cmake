578c955eb791f4c70155d3c31a097c54d0fcc26a
h1kk4 <ipodmacair@gmail.com>
2017-11-05 17:06:30 +0300

    added print package

---
+++ cmake/projects/print/hunter.cmake
@@ -0,0 +1,27 @@
+include(hunter_add_version)
+include(hunter_cacheable)
+include(hunter_cmake_args)
+include(hunter_download)
+include(hunter_pick_scheme)
+
+hunter_add_version(
+    PACKAGE_NAME
+    print
+    VERSION
+    "0.1.0.0"
+    URL
+    "https://github.com/h1kk4/lab09/archive/v0.1.0.0.tar.gz"
+    SHA1
+    b6c3dd2e3da63ee5a44e0a488d23e0ca1e724289
+)
+
+hunter_pick_scheme(DEFAULT url_sha1_cmake)
+
+hunter_cmake_args(
+    print
+    CMAKE_ARGS
+    BUILD_EXAMPLES=NO
+    BUILD_TESTS=NO
+)
+hunter_cacheable(print)
+hunter_download(PACKAGE_NAME print)
